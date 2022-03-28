d396fdc08b50517f5f7cabf3b7674fc67fe8990d
Angela Sun <angelasun1995@gmail.com>
2022-03-27 22:30:18 -0400

    Finish add book functionality

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("connected!");
+let myLibrary = [];
+
+function Book(title, author, pages) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+}
+
+function addBookToLibrary(
+  title = prompt("Title"),
+  author = prompt("Author"),
+  pages = prompt("Pages")
+) {
+  myLibrary.push(new Book(title, author, pages));
+  alert(myLibrary);
+}
+
+myLibrary.push(new Book("Game of Thrones", "George R.R. Martin", 429));
+addBookToLibrary();
