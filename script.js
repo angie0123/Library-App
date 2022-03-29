4e40c97f4084303b762c486483b1204b75335d93
Angela Sun <angelasun1995@gmail.com>
2022-03-28 20:57:48 -0400

    Add library rendering logic

--- script.js
+++ script.js
@@ -15,5 +15,23 @@ function addBookToLibrary(
   alert(myLibrary);
 }
 
-myLibrary.push(new Book("Game of Thrones", "George R.R. Martin", 429));
-addBookToLibrary();
+myLibrary.push(
+  new Book("A Game of Thrones", "George R.R. Martin", 694),
+  new Book("To Kill a Mockingbird", "Harper Lee", 281)
+);
+
+function renderLibrary() {
+  const library = document.querySelector(".library");
+  for (let book of myLibrary) {
+    library.appendChild(renderBook(book));
+  }
+}
+
+function renderBook(book) {
+  const card = document.createElement("div");
+  card.classList.add("book");
+  card.textContent = `${book.title} by ${book.author}; ${book.pages} pages`;
+  return card;
+}
+
+renderLibrary();
